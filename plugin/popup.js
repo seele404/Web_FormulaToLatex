3c9df12aef98a18ba135a09c970ce9cbe2f89590
seele404 <seelevollerei404@gmail.com>
2024-02-26 01:04:17 +0800

    [修改]转化功能修改，可以正常使用，但是恢复功能还有点瑕疵

--- plugin/popup.js
+++ plugin/popup.js
@@ -1,63 +1,81 @@
-// 当点击"disable-all-tex"按钮时，执行禁用 Katex 渲染的函数，并关闭当前窗口
+// popup.js
+
 document.getElementById('disable-all-tex').addEventListener('click', () => {
   chrome.tabs.executeScript({
-    // 执行禁用 Katex 渲染的函数五次，以确保覆盖所有 Katex 元素
-    code: '(' + disableKatexRendering.toString() + ')();'+
-    '(' + disableKatexRendering.toString() + ')();'+
-    '(' + disableKatexRendering.toString() + ')();'+
-    '(' + disableKatexRendering.toString() + ')();'+
-    '(' + disableKatexRendering.toString() + ')();',
+    code: `(${disableKatexRendering.toString()})(); (${observeDOMChanges.toString()})();`,
   });
   window.close(); // 关闭当前窗口
 });
 
-// 当点击"disable-katex-mathtype"按钮时，执行禁用 Katex 渲染（不包括分隔符）的函数，并关闭当前窗口
-document.getElementById('disable-katex-mathtype').addEventListener('click', () => {
+document.getElementById('restore-all-tex').addEventListener('click', () => {
   chrome.tabs.executeScript({
-    // 执行禁用 Katex 渲染（不包括分隔符）的函数五次，以确保覆盖所有 Katex 元素
-    code: '(' + disableKatexRenderingWithoutDelimiters.toString() + ')();'+
-    '(' + disableKatexRenderingWithoutDelimiters.toString() + ')();'+
-    '(' + disableKatexRenderingWithoutDelimiters.toString() + ')();'+
-    '(' + disableKatexRenderingWithoutDelimiters.toString() + ')();'+
-    '(' + disableKatexRenderingWithoutDelimiters.toString() + ')();',
+    code: `(${restoreKatexRendering.toString()})();`,
   });
   window.close(); // 关闭当前窗口
 });
 
-// 禁用 Katex 渲染并显示 LaTeX 公式的源代码
-function disableKatexRendering() {
-  const katexMathmlElements = document.getElementsByClassName('katex-mathml');
-  for (let i = 0; i < katexMathmlElements.length; i++) {
-    const katexMathmlElement = katexMathmlElements[i];
-    const annotationElement = katexMathmlElement.querySelector('annotation');
-    const sourceCode = annotationElement.textContent;
+function disableKatexRendering(depth = 0) {
+  const maxDepth = 5; // 设置最大递归深度，以避免无限循环
+  const katexElements = document.getElementsByClassName('katex');
+  if (katexElements.length === 0 || depth > maxDepth) {
+    return; // 如果没有元素需要处理或达到递归深度，停止执行
+  }
 
-    // 构建带有$符的 LaTeX 公式源代码
-    const formattedSourceCode = '$' + sourceCode + '$';
-    const sourceCodeNode = document.createTextNode(formattedSourceCode);
-    const katexHtmlElement = katexMathmlElement.nextElementSibling;
-    const katexElement = katexHtmlElement.parentElement;
+  let originalKatexHTML = [];
 
-    // 将 Katex 元素替换为 LaTeX 公式的源代码节点
-    katexElement.replaceWith(sourceCodeNode);
+  for (let i = 0; i < katexElements.length; i++) {
+    const katexElement = katexElements[i];
+    const katexHTML = katexElement.outerHTML;
+    originalKatexHTML.push(katexHTML);
+
+    const katexMathmlElement = katexElement.querySelector('.katex-mathml');
+    const annotationElement = katexMathmlElement ? katexMathmlElement.querySelector('annotation') : null;
+    if (annotationElement) {
+      const sourceCode = annotationElement.textContent;
+      const formattedSourceCode = '$' + sourceCode + '$';
+      katexElement.outerHTML = formattedSourceCode;
+    }
+  }
+
+  chrome.storage.local.get('originalKatexHTML', function(data) {
+    const storedHTML = data.originalKatexHTML || [];
+    const updatedHTML = storedHTML.concat(originalKatexHTML);
+    chrome.storage.local.set({'originalKatexHTML': updatedHTML}, function() {
+      console.log('Katex HTML is saved.');
+    });
+  });
+
+  // 递归调用自身，以尝试处理新的或遗漏的KaTeX元素
+  if (depth < maxDepth) {
+    disableKatexRendering(depth + 1);
   }
 }
 
-// 禁用 Katex 渲染并显示 LaTeX 公式的源代码（不包括分隔符）
-function disableKatexRenderingWithoutDelimiters() {
-  const katexMathmlElements = document.getElementsByClassName('katex-mathml');
-  for (let i = 0; i < katexMathmlElements.length; i++) {
-    const katexMathmlElement = katexMathmlElements[i];
-    const annotationElement = katexMathmlElement.querySelector('annotation');
-    const sourceCode = annotationElement.textContent;
+function observeDOMChanges() {
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      if (mutation.addedNodes.length) {
+        disableKatexRendering(); // 不需要递归深度，observeDOMChanges保证在DOM变化时触发
+      }
+    });
+  });
 
-    // 构建不带分隔符的 LaTeX 公式源代码
-    const sourceCodeNode = document.createTextNode(sourceCode);
-    const katexHtmlElement = katexMathmlElement.nextElementSibling;
-    const katexElement = katexHtmlElement.parentElement;
-
-    // 将 Katex 元素替换为 LaTeX 公式的源代码节点
-    katexElement.replaceWith(sourceCodeNode);
-  }
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true
+  });
 }
 
+function restoreKatexRendering() {
+  // 从chrome.storage中检索originalKatexHTML
+  chrome.storage.local.get('originalKatexHTML', function(data) {
+    const originalKatexHTML = data.originalKatexHTML || [];
+    const sourceCodes = document.body.querySelectorAll(':contains("$"):not(script):not(noscript):not(style)');
+    
+    sourceCodes.forEach((node, index) => {
+      if (originalKatexHTML[index]) {
+        node.outerHTML = originalKatexHTML[index]; // 使用存储的HTML恢复元素
+      }
+    });
+  });
+}
